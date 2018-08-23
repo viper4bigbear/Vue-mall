84d35bbc8874fb7cec60683892f026af2995b530
Bill <1311967697@qq.com>
2018-08-23 11:51:27 +0800

    top animation

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@
                             </a>
                         </li>
                         <li class="goods">
-                            <a href="" class="router-link-exact-active ">
+                            <a href="" class="">
                                 <span class="out" style="top: 0px;">购物商城</span>
                             </a>
                         </li>
@@ -114,12 +114,31 @@
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
-  name:"content"
+  name:"container"
 }
+$(document).ready(function() {
+	$("#menu2 li a").wrapInner( '<span class="out"></span>' );
+	$("#menu2 li a").each(function() {
+		$( '<span class="over">' +  $(this).text() + '</span>' ).appendTo( this );
+	});
+
+	$("#menu2 li a").hover(function() {
+		$(".out",	this).stop().animate({'top':	'48px'},	300); // move down - hide
+		$(".over",	this).stop().animate({'top':	'0px'},		300); // move down - show
+
+	}, function() {
+		$(".out",	this).stop().animate({'top':	'0px'},		300); // move up - show
+		$(".over",	this).stop().animate({'top':	'-48px'},	300); // move up - hide
+	});
+
+});
+
 </script>
 
 <style>
-  @import url('./assets/statics/site/css/style.css');
+@import url('./assets/statics/site/css/style.css');
+@import url('./assets/lib/css/style.css');
 </style>
 
