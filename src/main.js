4a8546b3ec8099c175cb9a5b7fd07046ff363123
Bill <1311967697@qq.com>
2018-08-23 19:47:18 +0800

    wrapper start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './app.vue'
+import Index from './components/index.vue'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+  {path: '/', redirect: '/index'},
+  {path: '/index', component: Index}
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
