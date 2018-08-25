c3d839917e96ffa905a1294647b82155bb74e679
Bill <1311967697@qq.com>
2018-08-25 14:58:55 +0800

    2018-8-24 detail

--- src/main.js
+++ src/main.js
@@ -5,18 +5,33 @@ import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import VueLazyload from 'vue-lazyload'
+
+import moment from 'moment'
 import App from './App.vue'
 
 import Index from './components/index.vue'
+import Detail from './components/detail.vue'
 
 Vue.use(VueRouter)
 Vue.use(ElementUI)
-
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: require('./assets/statics/886859.jpg'),
+  attempt: 1
+})
+Vue.filter(
+  'filterDate', function (val) {
+    return moment(val).format('YYYY年MM月DD日')
+  }
+)
 Vue.config.productionTip = false
 
 const routes = [
   {path: '/', redirect: '/index'},
-  {path: '/index', component: Index}
+  {path: '/index', component: Index},
+  {path: '/detail/:goodsid', component: Detail}
 ]
 
 const router = new VueRouter({
